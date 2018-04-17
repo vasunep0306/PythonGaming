eccd40298803a48821c65bf9876619ea46613af0
Vasanth Neppalli <neppallv@my.canisius.edu>
2018-04-17 17:15:21 -0400

    Setting Up the pygame Window

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,3 +3,7 @@ from pygame.locals import *
 
 #set up pygame
 pygame.init()
+
+#Set up the window
+windowSurface = pygame.display.set_mode((500,400), 0, 32)
+pygame.display.set_caption('Hello World')
