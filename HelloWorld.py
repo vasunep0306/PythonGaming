3e8e019f384931363c001d9b13be5bd690e446f5
Vasanth Neppalli <neppallv@my.canisius.edu>
2018-04-17 17:23:34 -0400

    Writing Text on the pygame Window

--- HelloWorld.py
+++ HelloWorld.py
@@ -7,3 +7,16 @@ pygame.init()
 #Set up the window
 windowSurface = pygame.display.set_mode((500,400), 0, 32)
 pygame.display.set_caption('Hello World')
+
+#Set up the colors
+colors = dict()
+colors["BLACK"] = (0,0,0)
+colors["WHITE"] = (255,255,255)
+colors["RED"] = (255,0,0)
+colors["GREEN"] = (0,255,0)
+colors["BLUE"] = (0,0,255)
+colors["GRAY"] = (128,128,128)
+
+# Set up the fonts
+basicFont = pygame.font.SysFont(None, 48)
+
